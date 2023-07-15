f877c71b590cf88e45c76c735588a9c2aabd84c2
Carl Dungca <carl.dungca@yahoo.com>
2023-07-15 13:33:11 -0700

    Adds barebones non-functional form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+
+// Your goal: Create an MVP
+//  Input a ZIP code
+//  Output data from the Yelp API
+// 1 activity, 1 dinner, 1 dessert
+// Output data from google maps
+// A link that can be sent to your phone
+
 function App() {
   return (
-    <div>
-      <h1>Where do you want to eat?</h1>
-      <p>I don't know, you choose.</p>
-    </div>
+    <Flex
+      flexDirection="column"
+      borderWidth="2px"
+      borderRadius="lg"
+      m={12}
+      p={5}
+      gap={5}
+    >
+      <Heading>You: What do you want to do tonight?</Heading>
+      <Heading>Your GF: Hmmmmm, IDK. You choose.</Heading>
+      <Heading>You:</Heading>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log('foo');
+        }}
+      >
+        <FormControl>
+          <FormLabel>Enter your ZIP code</FormLabel>
+          <Input />
+          <Button type="submit" mt={3} colorScheme="teal">
+            Plan my date for me
+          </Button>
+        </FormControl>
+      </form>
+    </Flex>
   );
 }
 
