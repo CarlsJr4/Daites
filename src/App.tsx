c7d2f3b4fae69d5e75b782183db6eb25c42e6ebf
Carl Dungca <carl.dungca@yahoo.com>
2023-07-17 20:11:45 -0700

    Moves map state logic to CustomMap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Heading, Text } from '@chakra-ui/react';
 
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import LocationInfoType from './types/locationType';
-import GeoJsonType from './types/geoJsonType';
 import BaseContainer from './components/BaseContainer';
 import ZipForm from './components/ZipForm';
 import Reccomendations from './components/Reccomendations';
 import pickRandomArrayItems from './helpers/pickRandomArrayItems';
 import CustomMap from './components/CustomMap';
 import './main.css';
-import MapType from './types/mapType';
 
 function App() {
   const [zip, setZip] = useState('');
@@ -21,41 +19,6 @@ function App() {
   const [filteredDateIdeas, setFilteredDateIdeas] = useState(
     [] as LocationInfoType[]
   );
-
-  const [markerCoords, setMarkerCoords] = useState<Array<number[]>>([[0, 0]]);
-  const [pathLine, setPathLine] = useState<MapType>({} as MapType);
-
-  // We can only run the mapbox API once we get the coordinates of the Yelp data. So we use an effect hook.
-  useEffect(() => {
-    if (filteredDateIdeas.length > 0) {
-      const mapboxToken = import.meta.env.VITE_MAPBOX as string;
-      const mapboxMarkerCoords: Array<number[]> = [];
-      let mapboxLinePathData = '';
-      filteredDateIdeas.forEach((date, i) => {
-        const latitude = date.coordinates.latitude;
-        const longitude = date.coordinates.longitude;
-
-        mapboxMarkerCoords.push([longitude, latitude]);
-        mapboxLinePathData += `${longitude},${latitude}`;
-        if (i + 1 < filteredDateIdeas.length) mapboxLinePathData += ';';
-      });
-
-      setMarkerCoords(mapboxMarkerCoords);
-
-      const mapboxEndpoint = `https://api.mapbox.com/directions/v5/mapbox/driving/${mapboxLinePathData}?geometries=geojson&access_token=${mapboxToken}`;
-      axios
-        .get<GeoJsonType>(mapboxEndpoint)
-        .then(res => {
-          const data = res.data.routes[0];
-          setPathLine({
-            pathArray: data.geometry.coordinates,
-            duration: data.duration,
-            distance: data.distance,
-          });
-        })
-        .catch(err => console.log(err));
-    }
-  }, [filteredDateIdeas]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -116,7 +79,7 @@ function App() {
         </BaseContainer>
       )}
       <BaseContainer>
-        <CustomMap pathLine={pathLine} markerCoords={markerCoords} />
+        <CustomMap locations={filteredDateIdeas} />
       </BaseContainer>
     </>
   );
--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -9,16 +9,19 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { Box, Heading, Text } from '@chakra-ui/react';
 import MapType from '../types/mapType';
 import { Offset } from 'mapbox-gl';
-
-type CustomMapProps = {
-  markerCoords: number[][];
-  pathLine: MapType;
-};
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import GeoJsonType from '../types/geoJsonType';
+import LocationInfoType from '../types/locationType';
 
 export default function CustomMap({
-  markerCoords: locationData,
-  pathLine,
-}: CustomMapProps) {
+  locations,
+}: {
+  locations: LocationInfoType[];
+}) {
+  const [markerCoords, setMarkerCoords] = useState<Array<number[]>>([[0, 0]]);
+  const [pathLine, setPathLine] = useState<MapType>({} as MapType);
+
   const pathDataCustom = {
     type: 'Feature',
     properties: {},
@@ -27,6 +30,38 @@ export default function CustomMap({
       coordinates: pathLine.pathArray,
     },
   };
+
+  // We can only run the mapbox API once we get the coordinates of the Yelp data. So we use an effect hook.
+  useEffect(() => {
+    if (locations.length > 0) {
+      const mapboxToken = import.meta.env.VITE_MAPBOX as string;
+      const mapboxMarkerCoords: Array<number[]> = [];
+      let mapboxLinePathData = '';
+      locations.forEach((date, i) => {
+        const latitude = date.coordinates.latitude;
+        const longitude = date.coordinates.longitude;
+
+        mapboxMarkerCoords.push([longitude, latitude]);
+        mapboxLinePathData += `${longitude},${latitude}`;
+        if (i + 1 < locations.length) mapboxLinePathData += ';';
+      });
+
+      setMarkerCoords(mapboxMarkerCoords);
+
+      const mapboxEndpoint = `https://api.mapbox.com/directions/v5/mapbox/driving/${mapboxLinePathData}?geometries=geojson&access_token=${mapboxToken}`;
+      axios
+        .get<GeoJsonType>(mapboxEndpoint)
+        .then(res => {
+          const data = res.data.routes[0];
+          setPathLine({
+            pathArray: data.geometry.coordinates,
+            duration: data.duration,
+            distance: data.distance,
+          });
+        })
+        .catch(err => console.log(err));
+    }
+  }, [locations]);
 
   return (
     <>
@@ -52,8 +87,8 @@ export default function CustomMap({
           latitude: 0,
           zoom: 10,
         }}
-        longitude={locationData[0][0]} // [0][0] is the longitude of the first location
-        latitude={locationData[0][1]} // [0][1] is the latitude of the first location
+        longitude={markerCoords[0][0]} // [0][0] is the longitude of the first location
+        latitude={markerCoords[0][1]} // [0][1] is the latitude of the first location
         style={{ width: 600, height: 400 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
@@ -73,13 +108,14 @@ export default function CustomMap({
             }}
           />
         </Source>
-        {locationData.map(location => {
+        {locations.map(location => {
+          const { longitude, latitude } = location.coordinates;
           return (
             <>
               <Popup
                 className="customPopup"
-                longitude={location[0]}
-                latitude={location[1]}
+                longitude={longitude}
+                latitude={latitude}
                 anchor="left"
                 offset={[15, -35] as Offset}
                 closeButton={false}
@@ -88,8 +124,8 @@ export default function CustomMap({
                 PLACEHOLDER
               </Popup>
               <Marker
-                longitude={location[0]}
-                latitude={location[1]}
+                longitude={longitude}
+                latitude={latitude}
                 anchor="bottom"
               />
             </>
